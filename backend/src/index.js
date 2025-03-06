1369cb5a3c0c5cc7d55b54e6ad9eef48c451e244
TitanProgrammer4480 <sebi-2007@web.de>
2025-03-06 15:03:05 +0100

    project completed

---
+++ backend/src/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+
+import { connectDB } from "./lib/db.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config()
+
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+
+server.listen(process.env.PORT, () => {
+    console.log(`server is running on ${process.env.PORT}`);
+    connectDB();
+});
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,101 @@
+import { create } from "zustand";
+
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+import { io } from "socket.io-client";
+
+export const useAuthStore = create((set, get) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+    onlineUsers: [],
+    socket: null,
+
+    checkAuth: async () => {
+        try {
+            const response = await axiosInstance.get("/auth/check");
+
+            set({ authUser: response.data });
+            get().connectSocket();
+        }catch (err) {
+            console.log("Error in checkAuth" + err);
+            set({ authUser: null });
+        }finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true });
+      try {
+        const res = await axiosInstance.post("/auth/signup", data);
+        set({ authUser: res.data });
+        toast.success("Account created successfully");
+        get().connectSocket();
+      } catch (err) {
+        toast.error(err.response.data.message);
+      }finally {
+        set({ isSigningUp: false});
+      }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Logged in successfully");
+            get().connectSocket();
+        }catch (err) {
+            toast.error(err.response.data.message);
+        }finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+            get().disconnectSocket();
+        }catch (err) {
+            toast.error(err.response.data.message);
+        }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("Profile updated successfully");
+        }catch (err) {
+            toast.error(err.response.data.message);
+        }finally {
+            set({ isUpdatingProfile:false });
+        }
+    },
+
+    connectSocket: () => {
+        const { authUser } = get();
+        if(!authUser  || get().socket?.connected) return;
+
+        const socket = io(import.meta.env.MODE === "development" ? "http://localhost:5001" : "/", {
+            query: {
+                userId: authUser._id,
+            }
+        });
+        socket.connect();
+        set({ socket: socket });
+        socket.on("getOnlineUsers", (userIds) => {
+            set({ onlineUsers: userIds });
+        });
+    },
+    disconnectSocket: () => {
+        if(get().socket?.connected) get().socket.disconnect();
+    },
+}));
